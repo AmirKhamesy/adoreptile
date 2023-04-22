e38d3101c5c04ca37d24af66193a351c6c3fe1b1
Amir <iknubs@gmail.com>
2023-04-22 15:10:25 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Adoreptile</title>
+        <meta name="description" content="Online reptile store" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>Adoreptile</p>
+      </main>
+    </>
+  )
+}
